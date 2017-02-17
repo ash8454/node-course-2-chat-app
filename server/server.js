efc7dbda7db304325fbaef8f699a9395fc75dee7
Ashok Tulachan <tulachanashok@Ashoks-MBP.fios-router.home>
2017-02-16 21:36:28 -0500

    Updated

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
 const publicPath = path.join(__dirname, '../public');
-console.log(__dirname + '/../public');
-console.log(publicPath);
+// console.log(__dirname + '/../public');
+// console.log(publicPath);
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+  console.log('New User connected');
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
